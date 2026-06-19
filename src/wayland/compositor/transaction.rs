5b6d84bb21060f11e18a420adec817d6c2fe6566
Skygrango <skygrango@gmail.com>
2026-06-19 18:53:57 +0800

    only blocker_cleared use calloop

--- src/wayland/compositor/transaction.rs
+++ src/wayland/compositor/transaction.rs
@@ -258,7 +258,8 @@ impl Transaction {
             })
     }
 
-    pub(crate) fn apply(self, dh: &DisplayHandle) {
+    pub(crate) fn apply(self, dh: &DisplayHandle) -> Vec<WlSurface> {
+        let mut committed = Vec::new();
         for (surface, id) in self.surfaces {
             let Ok(surface) = surface.upgrade() else {
                 continue;
@@ -268,12 +269,14 @@ impl Transaction {
                 states.cached_state.apply_state(id, dh);
             });
 
-            tracing::trace!("Emitting CompositorEvent::Commit for wl_surface");
-            if let Some(user_data) = surface.data::<super::SurfaceUserData>() {
-                let _ = user_data.tx_sender.send(super::CompositorEvent::Commit(surface.clone()));
-                //let _ = user_data.ping.ping();
-            }
+            // tracing::trace!("Emitting CompositorEvent::Commit for wl_surface");
+            // if let Some(user_data) = surface.data::<super::SurfaceUserData>() {
+            //     let _ = user_data.tx_sender.send(super::CompositorEvent::Commit(surface.clone()));
+            //     //let _ = user_data.ping.ping();
+            // }
+            committed.push(surface);
         }
+        committed
     }
 }
 
